e1c202a94c389e5148a5e82aa386c73d55959730
rgerbasi <31828557+rgerbasi@users.noreply.github.com>
2025-05-28 18:16:29 +0000

    feat: add mouseenter color change for square

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 // Javascript
-console.log("test")
+console.log("test");
 
 //create 16x16 grid of square divs
-const container = document.querySelector("div.container")
+const container = document.querySelector("div.container");
+
 //vertical flex container to stack rows
 for (let i = 0; i < 16; i++){
     //row
-    let row = document.createElement("div")
-    row.classList.add("row")
+    let row = document.createElement("div");
+    row.classList.add("row");
     for (let i = 0; i < 16; i++){
-        let col = document.createElement("div")
-        col.classList.add("square")
-        row.appendChild(col)
+        let col = document.createElement("div");
+        col.classList.add("square");
+        col.addEventListener("mouseenter", mouseInSquare) // mouse in square
+        row.appendChild(col);
     }
-    container.appendChild(row)
+    container.appendChild(row);
 }
 
+container.addEventListener("click", (e) => {
+    console.log(e.target.classList.value);
+});
+let test = document.querySelector(".square")
+function mouseInSquare(e) {
+    console.log(this.style)
+    const square = this
+    this.style["background-color"] = "black"
+
+}
+//best practice is to add mousemove when you need and remove when you dont need it
+//add mousemove event to mouseenter event and remove on mouse leave
+
+//event.target.classList is DOMTokenList = class of is in .value
+// container.removeEventListener()
+
+//not what im going for i think
+// function handleMouseMove(e) {
+//     console.log(e.target);
+//     if (e.target.classList.value === "square"){
+//         console.log("YEE")
+//     }
+// }
+// //on mouse enter add mousemove listener 
+// container.addEventListener("mouseenter", (e) => {
+//     container.addEventListener("mousemove", handleMouseMove);
+// });
+// //on mouse leave remove mousemove listener
+// container.addEventListener("mouseleave", (e) => {
+//     container.removeEventListener("mousemove", handleMouseMove);
+// })
