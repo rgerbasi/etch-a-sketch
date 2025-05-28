60ae5d6f306ae451ffaf64b38be963aa3dd324b9
rgerbasi <31828557+rgerbasi@users.noreply.github.com>
2025-05-28 17:12:07 +0000

    feat: add creating square16x16 grid in js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 // Javascript
 console.log("test")
+
+//create 16x16 grid of square divs
+const container = document.querySelector("div.container")
+//vertical flex container to stack rows
+for (let i = 0; i < 16; i++){
+    //row
+    let row = document.createElement("div")
+    row.classList.add("row")
+    for (let i = 0; i < 16; i++){
+        let col = document.createElement("div")
+        col.classList.add("square")
+        row.appendChild(col)
+    }
+    container.appendChild(row)
+}
+
